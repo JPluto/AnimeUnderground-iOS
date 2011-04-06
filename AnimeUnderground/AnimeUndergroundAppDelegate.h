f6fe07ce115aadcdf8ba401c04f860d605b53542
Nacho Lopez <mrm@aunder.es>
2011-04-06 17:49:35 +0200

    Initial Commit

---
+++ AnimeUnderground/AnimeUndergroundAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AnimeUndergroundAppDelegate.h
+//  AnimeUnderground
+//
+//  Created by Nacho López Sais on 06/04/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AnimeUndergroundAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
---
+++ AnimeUnderground/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  AnimeUnderground
+//
+//  Created by Nacho López Sais on 06/04/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UITableViewController {
+
+}
+
+
+@end

2a5250d52e6f242cceb131a749f9ff7b55c8cd4d
Nacho Lopez <mrm@aunder.es>
2011-04-14 16:16:58 +0200

    series

---
+++ AnimeUnderground/Serie.h
@@ -0,0 +1,16 @@
+//
+//  Serie.h
+//  AnimeUnderground
+//
+//  Created by Nacho López Sais on 14/04/11.
+//  Copyright 2011 AUDev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface Serie : NSObject {
+    
+}
+
+@end
---
+++ AnimeUnderground/Serie.m
@@ -0,0 +1,14 @@
+//
+//  Serie.m
+//  AnimeUnderground
+//
+//  Created by Nacho López Sais on 14/04/11.
+//  Copyright 2011 AUDev. All rights reserved.
+//
+
+#import "Serie.h"
+
+
+@implementation Serie
+
+@end

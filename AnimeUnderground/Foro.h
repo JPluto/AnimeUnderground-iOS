912f9c796a2aa729bc5bb098221320c1bec18703
Nacho Lopez <mrm@aunder.es>
2011-04-18 19:56:58 +0200

    añadido readme

---
+++ AnimeUnderground/Foro.h
@@ -0,0 +1,16 @@
+//
+//  Foro.h
+//  AnimeUnderground
+//
+//  Created by Nacho López Sais on 17/04/11.
+//  Copyright 2011 AUDev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface Foro : NSObject {
+    
+}
+
+@end
---
+++ AnimeUnderground/Foro.m
@@ -0,0 +1,14 @@
+//
+//  Foro.m
+//  AnimeUnderground
+//
+//  Created by Nacho López Sais on 17/04/11.
+//  Copyright 2011 AUDev. All rights reserved.
+//
+
+#import "Foro.h"
+
+
+@implementation Foro
+
+@end
